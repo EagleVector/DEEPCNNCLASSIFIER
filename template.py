ea6b6cb8ea870275d14e434594054b92afb61617
Eagle_Vector <kumarsubhambili3@gmail.com>
2022-12-17 14:57:12 +0530

    template modified

--- template.py
+++ template.py
@@ -1,11 +1,13 @@
+#CREATING THE BASIC SKELETON FOR THE PROJECT
 import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ') #logging level set to info level
 
 package_name = "DeepClassifier"
 
+#creating all the necessary files for project in one go
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{package_name}/__init__.py"
@@ -26,19 +28,21 @@ list_of_files = [
     "pyproject.toml",
     "tox.ini",
     "research/trials.ipynb"
-]
+]                           
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir, filename = os.path.split(filepath)
+    filedir, filename = os.path.split(filepath)   #splits the diectory with the file
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for file: {filename}")
+        logging.info(f"Creating directory: {filedir} for file: {filename}") #creating directory
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0): #create file only when its an empty file otherwise skip: donot overwrite
         with open(filepath, "w") as f:
             pass
-            logging.info(f"Creating empty file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}") #creating files
 
     else:
         logging.info(f"{filename} already exists")
+
+#Necessary skelton created
