60f5739e51290b44d2a5a160957453bb28707f4f
Eagle_Vector <kumarsubhambili3@gmail.com>
2022-12-30 18:14:58 +0530

    stage_02_prepare_base_model created

---
+++ src/DeepClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,29 @@
+from DeepClassifier.components import PrepareBaseModel
+from DeepClassifier.config import ConfigurationManager
+from DeepClassifier import logger
+
+STAGE_NAME = "Prepare Base Model"
+
+
+def main():
+    config = ConfigurationManager()
+    prepare_base_model_config = config.get_prepare_base_model_config()
+    prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+    prepare_base_model.get_base_model()
+    prepare_base_model.update_base_model()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"********************************")
+        logger.info(
+            f"<<<< Stage {STAGE_NAME} started <<<<<\n\nx============x"
+        )
+
+        main()
+        logger.info(
+            f"<<<< Stage {STAGE_NAME} completed successfully <<<<<\n\nx============x"
+        )
+    except Exception as e:
+        logger.exception(e)
+        raise e
