088cd042ddf06f2cae4cae0f1d5b33d1f84b57b8
Eagle_Vector <kumarsubhambili3@gmail.com>
2022-12-30 19:11:48 +0530

    Training_Stage_added

--- src/DeepClassifier/entity/__init__.py
+++ src/DeepClassifier/entity/__init__.py
@@ -1,1 +1,1 @@
-from DeepClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig
+from DeepClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
