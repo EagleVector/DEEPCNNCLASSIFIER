b3de9dbbb3c31362775ca918d2e8a501f369e664
EagleVector <kumarsubhambili3@gmail.com>
2023-01-02 13:27:46 +0530

    evaluation stage added

---
+++ src/DeepClassifier/pipeline/stage_04_evaluation.py
@@ -0,0 +1,28 @@
+from DeepClassifier.components import Evaluation
+from DeepClassifier.config import ConfigurationManager
+from DeepClassifier import logger
+
+STAGE_NAME = "Evaluation"
+
+
+def main():
+    config = ConfigurationManager()
+    val_config = config.get_validation_config()
+    evaluation = Evaluation(val_config)
+    evaluation.evaluation()
+    evaluation.save_score()
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"********************************")
+        logger.info(
+            f"<<<< Stage {STAGE_NAME} started <<<<<\n\n"
+        )
+
+        main()
+        logger.info(
+            f"<<<< Stage {STAGE_NAME} completed successfully <<<<<\n\nx============x"
+        )
+    except Exception as e:
+        logger.exception(e)
+        raise e
